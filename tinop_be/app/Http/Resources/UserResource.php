b51b2df43e47b2f234974e5e31cc1a18c5fdcb10
Ondřej Hašek <140166751+HasekOn@users.noreply.github.com>
2025-05-05 18:16:25 +0200

    BE BUG FIX on user

--- tinop_be/app/Http/Resources/UserResource.php
+++ tinop_be/app/Http/Resources/UserResource.php
@@ -18,6 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'is_admin' => $this->is_admin,
             'email_verified_at' => $this->when($this->email_verified_at, function () {
                 return $this->email_verified_at->format('Y-m-d H:i:s');
             })
