890539f3c413ec0a5aa2ee18e0bdb3ba0819d6b8
Ondřej Hašek <140166751+HasekOn@users.noreply.github.com>
2025-05-05 23:26:31 +0200

    BE fix

--- tinop_be/app/Http/Resources/IdeaResource.php
+++ tinop_be/app/Http/Resources/IdeaResource.php
@@ -14,12 +14,18 @@ class IdeaResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        /** @var \App\Models\User|null $me */
+        $me = $request->user();
+
+        $isOwner = $me && $me->id === $this->user_id;
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'likes' => $this->likes,
             'dislikes' => $this->dislikes,
             'description' => $this->description,
+            'is_user_owner' => $isOwner,
         ];
     }
 }
